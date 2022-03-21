196b109fad43c591d7e20a83f453ffd90d7e94ef
Ansab Sidiki <ansab@sidiki.io>
2022-03-20 21:15:29 -0400

    defined possible variables in JS

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function initPage() {
+    var cityEl = document.getElementById("enter-city");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");
+    var currentWindEl = document.getElementById("wind-speed");
+    var currentUVEl = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    var apiKey = "fe30effa9c7c0df8b9f8f6502c202150";
+}
